c40387779f3e70d107c0976b77d9fb9aedced9ff
dimentiygg <sfx670@gmail.com>
2024-02-10 16:46:17 +0200

    done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,32 +1,29 @@
 const form = document.querySelector('.feedback-form');
 const storageKey = 'feedback-form-state';
 
-const data = form => {
+function data(form) {
   const email = form.email.value;
   const message = form.message.value;
   return {
     email,
     message,
   };
-};
+}
 
 form.addEventListener('input', event => {
   localStorage.setItem(storageKey, JSON.stringify(data(form)));
 });
 
-const dataFromStorage = localStorage.getItem(storageKey);
-
-if (dataFromStorage) {
-  const object = JSON.parse(dataFromStorage);
-  form.email.value = object.email;
-  form.message.value = object.message;
-}
+const getDataFromStorage = () => localStorage.getItem(storageKey);
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  if (localStorage) {
-    console.log(JSON.parse(dataFromStorage));
+  if (localStorage.length !== 0) {
+    const object = JSON.parse(getDataFromStorage());
+    form.email.value = object.email;
+    form.message.value = object.message;
+    console.log(JSON.parse(getDataFromStorage()));
+    localStorage.clear();
+    form.reset();
   }
-  localStorage.clear();
-  form.reset();
 });
